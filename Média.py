0be7a2dd7a9dad7b8f015fb9c13a28583583bd99
EmilyNicoleA <72318676+EmilyNicoleA@users.noreply.github.com>
2020-10-31 14:40:05 -0400

    Create Média.py

---
+++ Média.py
@@ -0,0 +1,12 @@
+num1 = float(input('Digite a primeira nota: '))
+num2 = float(input('Digite a segunda nota: '))
+num3 = float(input('Digite a terceira nota: '))
+num4 = float(input('Digite a quarta nota: '))
+
+soma = num1 + num2 + num3 + num4
+sub = num1 - num2 - num3 - num4
+media = soma / 4
+ 
+print(soma)
+print(sub)
+print(media)
